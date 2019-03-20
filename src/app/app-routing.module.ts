26d640364023145f668afef229d71cd81be0c149
ashok <as@mail.com>
2019-03-20 06:03:51 +0530

    login details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+const routes: Routes = [
+  { path : 'login', component : LoginComponent },
+  { path : 'register', component : RegisterComponent },
+  { path : '', component : LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RoutingComponent = [
+  LoginComponent,
+  RegisterComponent
+];
